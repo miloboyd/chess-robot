e26eef153277e98a6093cb0038b690a52b2681fc
Milo Boyd <milocb123@gmail.com>
2025-05-21 09:57:47 +1000

    minor changes

--- src/robot_manipulation/main.cpp
+++ src/robot_manipulation/main.cpp
@@ -6,7 +6,17 @@ int main(int argc, char** argv)
   rclcpp::init(argc, argv);
   auto node = std::make_shared<RobotControl>();
   // Run the demo
-  node->run_demo();
+  
+  //subscribe to a pose 
+  geometry_msgs::msg::Pose input_pose;
+  input_pose.position.x = 0;
+  input_pose.position.y = 0.2;
+  input_pose.position.z = 0.1;
+
+
+  node->moveRobot(input_pose.position.x, input_pose.position.y, input_pose.position.z);
+
+
   
   rclcpp::shutdown();
   return 0;
